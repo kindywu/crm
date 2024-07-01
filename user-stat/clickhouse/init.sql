295523b53e04c3e682f1d5c726f287d07558d0a1
kindywu <kindywu@outlook.com>
2024-07-01 15:55:04 +0800

    add user-stat

---
+++ user-stat/clickhouse/init.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS stats;
+
+CREATE TABLE user_stats (
+  email String,
+  name String,
+  gender Enum8('unknown' = 0, 'male' = 1, 'female' = 2) DEFAULT 'unknown',
+  created_at DateTime DEFAULT now(),
+  last_visited_at DateTime,
+  last_watched_at DateTime,
+  recent_watched Array(Int32),
+  viewed_but_not_started Array(Int32),
+  started_but_not_finished Array(Int32),
+  finished Array(Int32),
+  last_email_notification DateTime,
+  last_in_app_notification DateTime,
+  last_sms_notification DateTime
+) ENGINE = MergeTree()
+PRIMARY KEY email
+ORDER BY (email, created_at, last_visited_at, last_watched_at, last_email_notification, last_in_app_notification, last_sms_notification);
---
+++ user-stat/examples/gen_data_clickhouse.rs
@@ -0,0 +1,100 @@
+use anyhow::Result;
+use chrono::{DateTime, Days, Utc};
+use clickhouse::{Client, Row};
+use fake::{
+    faker::{chrono::en::DateTimeBetween, internet::en::SafeEmail, name::zh_cn::Name},
+    Dummy, Fake, Faker,
+};
+use nanoid::nanoid;
+use rand::Rng;
+use serde::Serialize;
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // let user_stat = Faker.fake::<UserStat>();
+    // println!("{user_stat:?}");
+    let client = Client::default()
+        .with_url("http://localhost:8123")
+        .with_database("stats");
+    let mut insert = client.insert("user_stats")?;
+    let user_stat = Faker.fake::<UserStat>();
+    insert.write(&user_stat).await?;
+    insert.end().await?;
+
+    println!("Inserted 10 records successfully!");
+
+    Ok(())
+}
+
+#[derive(Debug, Clone, Dummy, Serialize, Row, PartialEq, Eq)]
+struct UserStat {
+    #[dummy(faker = "UniqueEmail")]
+    email: String,
+    #[dummy(faker = "Name()")]
+    name: String,
+    gender: Gender,
+    #[dummy(faker = "DateTimeBetween(before(365*5), before(90))")]
+    created_at: DateTime<Utc>,
+    #[dummy(faker = "DateTimeBetween(before(30), now())")]
+    last_visited_at: DateTime<Utc>,
+    #[dummy(faker = "DateTimeBetween(before(90), now())")]
+    last_watched_at: DateTime<Utc>,
+    #[dummy(faker = "IntList(50, 100000, 100000)")]
+    recent_watched: Vec<i32>,
+    #[dummy(faker = "IntList(50, 200000, 100000)")]
+    viewed_but_not_started: Vec<i32>,
+    #[dummy(faker = "IntList(50, 300000, 100000)")]
+    started_but_not_finished: Vec<i32>,
+    #[dummy(faker = "IntList(50, 400000, 100000)")]
+    finished: Vec<i32>,
+    #[dummy(faker = "DateTimeBetween(before(45), now())")]
+    last_email_notification: DateTime<Utc>,
+    #[dummy(faker = "DateTimeBetween(before(15), now())")]
+    last_in_app_notification: DateTime<Utc>,
+    #[dummy(faker = "DateTimeBetween(before(90), now())")]
+    last_sms_notification: DateTime<Utc>,
+}
+// How to define enums that map to `Enum8`/`Enum16`.
+#[derive(Debug, Clone, Dummy, Serialize_repr, Deserialize_repr, PartialEq, Eq)]
+#[repr(u8)]
+enum Gender {
+    Female,
+    Male,
+    Unknown,
+}
+
+struct UniqueEmail;
+
+const ALPHABET: [char; 36] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+
+impl Dummy<UniqueEmail> for String {
+    fn dummy_with_rng<R: Rng + ?Sized>(_: &UniqueEmail, rng: &mut R) -> String {
+        let email: String = SafeEmail().fake_with_rng(rng);
+        let id = nanoid!(8, &ALPHABET);
+        let at = email.find('@').unwrap();
+        format!("{}.{}{}", &email[..at], id, &email[at..])
+    }
+}
+
+fn before(days: u64) -> DateTime<Utc> {
+    Utc::now().checked_sub_days(Days::new(days)).unwrap()
+}
+
+fn now() -> DateTime<Utc> {
+    Utc::now()
+}
+struct IntList(pub i32, pub i32, pub i32);
+
+impl Dummy<IntList> for Vec<i32> {
+    fn dummy_with_rng<R: Rng + ?Sized>(v: &IntList, rng: &mut R) -> Vec<i32> {
+        let (max, start, len) = (v.0, v.1, v.2);
+        let size = rng.gen_range(0..max);
+        (0..size)
+            .map(|_| rng.gen_range(start..start + len))
+            .collect()
+    }
+}
