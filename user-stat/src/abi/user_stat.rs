86d5f459b845caa6d0522f627c8541a95d3e7d3b
kindywu <kindywu@outlook.com>
2024-07-02 22:49:24 +0800

    features add new line to .tokeignore

--- user-stat/src/abi/user_stat.rs
+++ user-stat/src/abi/user_stat.rs
@@ -1,6 +1,7 @@
 use std::pin::Pin;
 
-use tokio_stream::Stream;
+use tokio_stream::{self as stream, Stream};
+
 use tonic::{Response, Status};
 
 use crate::{QueryRequest, RawQueryRequest, User, UserStatService};
@@ -14,8 +15,40 @@ impl UserStatService {
 
     pub async fn raw_query(
         &self,
-        _request: RawQueryRequest,
+        req: RawQueryRequest,
     ) -> Result<Response<ResponseStream>, Status> {
-        todo!()
+        let result: Result<Vec<User>, sqlx::Error> =
+            sqlx::query_as(&req.query).fetch_all(&self.state.pool).await;
+
+        match result {
+            Ok(users) => {
+                let stream = stream::iter(users.into_iter().map(Ok));
+                let response_stream = Box::pin(stream);
+                Ok(Response::new(response_stream))
+            }
+            Err(err) => Err(Status::internal(err.to_string())),
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+    use tokio_stream::StreamExt;
+
+    use crate::{AppConfig, RawQueryRequest, UserStatService};
+
+    #[tokio::test]
+    async fn raw_query_should_work() -> Result<()> {
+        let config = AppConfig::load().expect("Failed to load config");
+        let svc = UserStatService::new(config).await?;
+
+        let query = "select * from user_stats where created_at > '2024-01-01' limit 5".to_string();
+
+        let mut stream = svc.raw_query(RawQueryRequest { query }).await?.into_inner();
+
+        while let Some(Ok(res)) = stream.next().await {
+            println!("{:?}", res);
+        }
+        Ok(())
     }
 }
