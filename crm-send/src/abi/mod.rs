7c3d97d10560ac6920ab81f515074f4e5fb64e64
kindywu <kindywu@outlook.com>
2024-07-04 15:42:11 +0800

    commit

--- crm-send/src/abi/mod.rs
+++ crm-send/src/abi/mod.rs
@@ -81,49 +81,6 @@ mod tests {
     };
     use anyhow::Result;
 
-    impl EmailMessage {
-        pub fn fake() -> Self {
-            use fake::faker::internet::en::SafeEmail;
-            use fake::Fake;
-            use uuid::Uuid;
-            EmailMessage {
-                message_id: Uuid::new_v4().to_string(),
-                sender: SafeEmail().fake(),
-                recipients: vec![SafeEmail().fake()],
-                subject: "Hello".to_string(),
-                body: "Hello, world!".to_string(),
-            }
-        }
-    }
-
-    #[cfg(test)]
-    impl InAppMessage {
-        pub fn fake() -> Self {
-            use uuid::Uuid;
-            InAppMessage {
-                message_id: Uuid::new_v4().to_string(),
-                device_id: Uuid::new_v4().to_string(),
-                title: "Hello".to_string(),
-                body: "Hello, world!".to_string(),
-            }
-        }
-    }
-
-    #[cfg(test)]
-    impl SmsMessage {
-        pub fn fake() -> Self {
-            use fake::faker::phone_number::en::PhoneNumber;
-            use fake::Fake;
-            use uuid::Uuid;
-            SmsMessage {
-                message_id: Uuid::new_v4().to_string(),
-                sender: PhoneNumber().fake(),
-                recipients: vec![PhoneNumber().fake()],
-                body: "Hello, world!".to_string(),
-            }
-        }
-    }
-
     #[tokio::test]
     async fn send_should_work() -> Result<()> {
         let config = AppConfig::load()?;
--- crm-send/src/abi/sender.rs
+++ crm-send/src/abi/sender.rs
@@ -6,7 +6,6 @@ use crate::EmailMessage;
 use crate::InAppMessage;
 use crate::SmsMessage;
 
-#[allow(unused)]
 pub trait Sender {
     async fn send(&self) -> Result<String>;
 }
@@ -32,5 +31,48 @@ impl Sender for SmsMessage {
         println!("send {self:?}");
         sleep(Duration::from_secs(1)).await;
         Ok(self.message_id.clone())
+    }
+}
+
+impl EmailMessage {
+    pub fn fake() -> Self {
+        use fake::faker::internet::en::SafeEmail;
+        use fake::Fake;
+        use uuid::Uuid;
+        EmailMessage {
+            message_id: Uuid::new_v4().to_string(),
+            sender: SafeEmail().fake(),
+            recipients: vec![SafeEmail().fake()],
+            subject: "Hello".to_string(),
+            body: "Hello, world!".to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+impl InAppMessage {
+    pub fn fake() -> Self {
+        use uuid::Uuid;
+        InAppMessage {
+            message_id: Uuid::new_v4().to_string(),
+            device_id: Uuid::new_v4().to_string(),
+            title: "Hello".to_string(),
+            body: "Hello, world!".to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+impl SmsMessage {
+    pub fn fake() -> Self {
+        use fake::faker::phone_number::en::PhoneNumber;
+        use fake::Fake;
+        use uuid::Uuid;
+        SmsMessage {
+            message_id: Uuid::new_v4().to_string(),
+            sender: PhoneNumber().fake(),
+            recipients: vec![PhoneNumber().fake()],
+            body: "Hello, world!".to_string(),
+        }
     }
 }
